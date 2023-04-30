d770f283a9508ab7611255b0384c1ad529af933f
Merle <merle.xzhang@gmail.com>
2023-04-30 23:02:06 +0000

    Make Navigation Bar Material.

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -1,26 +1,47 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { Link as RouterLink } from "react-router-dom";
+
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
 
 const NavigationBar: React.FC = () => {
   return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <NavLink to="/">Index</NavLink>
-          </li>
-          <li>
-            <NavLink to="/home">Home</NavLink>
-          </li>
-          <li>
-            <NavLink to="/todo">Todo</NavLink>
-          </li>
-          <li>
-            <NavLink to="/finance">Finance</NavLink>
-          </li>
-        </ul>
-      </nav>
-    </div>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Current Page Title
+          </Typography>
+          <Button color="inherit" component={RouterLink} to="/">
+            Index
+          </Button>
+          <Button color="inherit" component={RouterLink} to="/home">
+            Home
+          </Button>
+          <Button color="inherit" component={RouterLink} to="/todo">
+            Todo
+          </Button>
+          <Button color="inherit" component={RouterLink} to="/finance">
+            Finance
+          </Button>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 };
 
